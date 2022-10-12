7011195f2d6f8b38d22c4d40689b5d9c1147d310
Slimane <80587317+slimanesedrati@users.noreply.github.com>
2022-10-12 15:34:27 +0200

    commit

--- advice-generator-app-main/index.js
+++ advice-generator-app-main/index.js
@@ -1,5 +1,21 @@
-
-
 const adviceId = document.getElementById("advice-id")
-const adviceText = document.querySelector("advice-text")
+const adviceText = document.querySelector(".advice-text")
 const dice = document.getElementById("dice")
+
+window.onload = getQuote;
+
+dice.addEventListener("click", getQuote)
+
+
+function getQuote() {
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then((data) => data.slip)
+    .then((data) => {
+        adviceId.innerText = data.id;
+        adviceText.innerHTML = `“${data.advice}”`;
+    })
+    .catch((error) => {
+        alert(`Error ${error}`)
+    })
+}
